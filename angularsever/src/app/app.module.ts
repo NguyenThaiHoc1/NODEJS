640eb5f7669130041030394c011777b87b55d073
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2020-03-04 05:47:44 +0700

    tao 3 component trong thu muc component/<Noi Dung> bang cau lenh ng g component <>

--- angularsever/src/app/app.module.ts
+++ angularsever/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddIssueComponent } from './components/add-issue/add-issue.component';
+import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
